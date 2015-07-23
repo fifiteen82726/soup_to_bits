23d27962582a8454c18c7a1afe064cb4bf7d6135
fifiteen82726 <fifiteen82726@gmail.com>
2015-07-23 16:13:10 +0800

    finish project, add admin to toggle feactured, add session[:vip] to get secret_soups

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,7 +1,12 @@
 class SoupsController < ApplicationController
-	def show
-		@soup  = Soup.find (params[:id])
+	before_action :fetch_soup, only: [:show, :edit, :update, :destory, :toggle_featured]
 
+	def fetch_soup
+		@soup  = Soup.find (params[:id])
+	end
+
+	def show
+	
 		respond_to do |format|
 			format.html 
 			format.json { render json: @soup}
@@ -15,6 +20,33 @@ class SoupsController < ApplicationController
 			format.html 
 			format.json { render json: @soup }
 		end
+	end
+
+	def new
+		
+	end
+
+	def efit
+		
+	end
+
+	def update
+		
+	end
+
+	def destory
+		
+	end
+
+	def toggle_featured
+		@soup.toggle!(:featured)
+		flash[:notice] = "successfully change"
+		redirect_to @soup
+	end
+
+	def secret_soups
+		session[:vip] = true
+		redirect_to root_path
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
   resources :categories, :soups
+
+  root 'categories#index'
+
+  get 'allcategories', to: redirect('/')
+
+  get '/soups/:id/toggle_featured', to: 'soups#toggle_featured', as: :toggle_featured
+
+  get 'secret_soups' => 'soups#secret_soups' 
+  #get 'allcategories' => 'categories#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
