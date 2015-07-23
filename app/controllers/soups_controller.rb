f41d1b80840cd32360c85f881ae7eb376f710774
fifiteen82726 <fifiteen82726@gmail.com>
2015-07-23 15:30:01 +0800

    add index ,show json format

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,5 +1,20 @@
 class SoupsController < ApplicationController
 	def show
 		@soup  = Soup.find (params[:id])
+
+		respond_to do |format|
+			format.html 
+			format.json { render json: @soup}
+		end
 	end
+
+	def index
+		@soup = Soup.all
+
+		respond_to do |format|
+			format.html 
+			format.json { render json: @soup }
+		end
+	end
+
 end
